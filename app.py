2427e7a9496bda76e27960f54e98183203d233cb
karan-128 <100510785+karan-128@users.noreply.github.com>
2022-11-26 03:01:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 import streamlit as st
-import pandas as pd
+
 st.write("""
 # Graded Assignment 8 - Multiplication App
 This app simply multiplies 2 numbers
 """)
+
 #Get Input
 st.header('User Input Parameters')
 
-def user_input_features():
-    num1 = st.number_input("Number 1")
-    num2 = st.number_input("Number 2")
-df = user_input_features()	
-
-st.subheader('User Input parameters')
+num1 = st.number_input("Number 1")
+num2 = st.number_input("Number 2")
 
 def multiply(a,b):
 	return float(a)*float(b)
-
-st.write(multiply(df['num1'],df['num2']))
+n1=num1
+n2=num2
+result=multiply(n1,n2)
+st.subheader('Result')
+st.write('result')
