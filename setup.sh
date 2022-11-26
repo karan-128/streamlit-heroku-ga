a924e41084595e6b8b3dffbc574b462edada7a55
karan-128 <100510785+karan-128@users.noreply.github.com>
2022-11-26 14:01:26 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"21f2000771@student.onlinedegree.iitm.ac.in\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
